54b5da36af006d0a6bbd7b810c6ac3d142bdb142
Jory Leech <joryleech@gmail.com>
2018-09-25 22:35:37 -0700

    Added stack functions, moved accessability of GameState, begun parental relation implementation

--- SDL-Basic-Setup/gameController.cpp
+++ SDL-Basic-Setup/gameController.cpp
@@ -1,6 +1,6 @@
 #include "gameController.h"
 
-GameController::GameController(JEngine * engine,GameState * defaultState)
+GameController::GameController(JEngine * engine,GameController::GameState * defaultState)
 {
 	this->defaultState = defaultState;
 	this->engine = engine;
@@ -15,34 +15,69 @@ void GameController::update()
 		this->pushState(defaultState);
 	}
 	else{
-		engine->debugPrint("GameState is set to null");
+		engine->debugPrint("GameController::GameState is set to null");
 	}
 }
 
+GameController::GameState * GameController::popState()
+{
+	GameController::GameState* oldState = this->gameStateStack.back();
+	this->gameStateStack.pop_back(); //Unlike standard pop this does not return the object
+	changeState(this->getCurrentState());
+	return oldState;
+}
+std::list<GameController::GameState*> GameController::popState(int count)
+{
+	std::list<GameController::GameState*> statesToReturn;
+	for (int i = 0; i < count && !gameStateStack.empty(); i++) {
+		statesToReturn.push_back(this->gameStateStack.back());
+		this->gameStateStack.pop_back();
+	}
+	changeState(this->getCurrentState());
+	return statesToReturn;
+}
 
-GameState * GameController::changeState(GameState * newState)
+GameController::GameState * GameController::replaceState(GameController::GameState * newState)
+{
+	GameController::GameState* oldState = this->gameStateStack.back();
+	this->gameStateStack.pop_back();
+	this->pushState(newState);
+	return oldState;
+}
+
+void GameController::pushState(GameController::GameState * newState)
+{
+	this->gameStateStack.push_back(newState);
+	changeState(getCurrentState());
+}
+
+
+void GameController::changeState(GameController::GameState * newState)
 {
 	//The Engine must remove the render texture of other screens, inorder to render this screen.
 	this->engine->getImageManager()->removeAllElements();
 	this->engine->addElement(getCurrentState()->getScreen());
 
-	//TODO: Remove return
-	return nullptr;
-	//return oldState;
+
 }
-GameState * GameController::getCurrentState() {
+GameController::GameState * GameController::getCurrentState() {
 	if (gameStateStack.empty()) {
 		return nullptr;
 	}
-	return gameStateStack.back;
+	return gameStateStack.back();
 }
 
-JRenderer * GameState::getScreen()
+JRenderer * GameController::GameState::getScreen()
 {
 	return this->screen;
 }
 
-void GameState::createScreen(int width, int height)
+void GameController::GameState::createScreen(int width, int height)
 {
 	this->screen = new JRenderer(0, 0, width, height);
+}
+
+void DefaultState::onStart()
+{
+	
 }
--- SDL-Basic-Setup/gameController.h
+++ SDL-Basic-Setup/gameController.h
@@ -6,29 +6,33 @@
 
 static int engineScreenWidth=1920;
 static int engineScreenHeight=1080;
-class GameState {
 
-public:
-	static enum stateModificaction {
-		None, Pop, Replace, Push, ReplaceAndDelete, PopAndDelete
-	};
-
-	stateModificaction modifyState = None;
-	GameState * stateModificactionpointer = NULL;
-	GameState();
-	virtual void update() {};
-	virtual void onStart() {};
-	virtual void onExit() {};
-	virtual void load() {};
-	virtual void postLoad() {};
-	JRenderer * getScreen();
-	void createScreen(int width, int height);
-protected:
-	JRenderer * screen;
-};
 
 class GameController {
-	std::list<GameState> gameStateStack;
+public:
+	class GameState {
+	public:
+		GameController * parent;
+		static enum stateModificaction {
+			None, Pop, Replace, Push, ReplaceAndDelete, PopAndDelete
+		};
+
+		stateModificaction modifyState = None;
+		GameState * stateModificactionpointer = NULL;
+		GameState();
+		virtual void update() {};
+		virtual void onStart() {};
+		virtual void onExit() {};
+		virtual void load() {};
+		virtual void postLoad() {};
+		JRenderer * getScreen();
+		void createScreen(int width, int height);
+	protected:
+		JRenderer * screen;
+	};
+
+
+	std::list<GameState *> gameStateStack;
 	GameController(JEngine * engine,GameState* defaultState);
 	GameController(JEngine * engine) : GameController(engine, nullptr) {};
 	void update();
@@ -36,13 +40,19 @@ class GameController {
 	JEngine * engine;
 	
 	GameState * popState();
+	std::list<GameState *> popState(int count);
 	GameState * replaceState(GameState * newState);
 	void pushState(GameState * newState);
 	void deleteState(GameState * state);
 	GameState * getCurrentState();
 private:
-	GameState * changeState(GameState * newState);
+   void changeState(GameState * newState);
 
 	GameState * defaultState;
+
 };
 #endif
+
+class DefaultState : GameController::GameState {
+	void onStart();
+};
